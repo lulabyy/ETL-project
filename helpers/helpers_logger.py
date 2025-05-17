3d572804dcc0a9a072a17933fde91117d2f5352c
lulabyy <lollaconne@gmail.com>
2025-05-17 18:40:08 +0200

    logger generique

--- helpers/helpers_logger.py
+++ helpers/helpers_logger.py
@@ -1,18 +1,38 @@
 import logging
+import os
 
-def setupLogger():
-    # The log allows tracking the execution of the code
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s | %(name)s | %(levelname)s | %(module)s | %(filename)s | %(funcName)s | %(lineno)d | %(message)s",
-        handlers=[
-            logging.StreamHandler(),  # Display the log in the console
-            logging.FileHandler("etl.log"),  # Write the log in a file
-        ],
-    )
+def setupLogger(name="etl_logger", log_dir="log", log_filename="etl.log"):
+    """
+    Initialise le logger et écrit les logs dans le dossier spécifié (par défaut 'log').
 
-    # Création du logger
-    logger = logging.getLogger("etl_logger")
-    logging.getLogger("sqlalchemy.engine.Engine").disabled = True
+    Parameters:
+    ----------
+    :param name: logger name
+    :param log_dir: dir name
+    :return: the logger
+    """
+    current_file = os.path.abspath(__file__)
+    project_root = os.path.dirname(os.path.dirname(current_file))
 
+    full_log_dir = os.path.join(project_root, log_dir)
+    os.makedirs(full_log_dir, exist_ok=True)
 
+    log_file = os.path.join(full_log_dir, log_filename)
+
+    # Création du logger si il existe pas déjà
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            "%(asctime)s | %(name)s | %(levelname)s | %(module)s | %(funcName)s | %(lineno)d | %(message)s"
+        )
+        
+        file_handler = logging.FileHandler(log_file, mode="w")
+        file_handler.setFormatter(formatter)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+    return logger
